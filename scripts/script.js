0f9ccf9e85b675a920ff1075d180e808638b020e
Misha Zhuravlev <mishami816@gmail.com>
2025-02-15 21:23:53 +0300

    Second-UpDate

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 //открытие меню
 function menu() {
+  document.getElementById("menuList").classList.toggle("open-flex");
   document.getElementById("menuList").classList.toggle("open");
   document.getElementById("page").classList.toggle("fix");
 }
@@ -31,3 +32,36 @@ var swiper = new Swiper('.swiper-container', {
       prevEl: '.swiper-button-prev',
   },
 });
+
+var swiper = new Swiper('.swiper-container2', {
+  slidesPerView: 1,
+  spaceBetween: 8,
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+});
+
+document.querySelectorAll('.services-btn-list').forEach(button => {
+  button.addEventListener('click', () => {
+      const list = button.nextElementSibling; // Получаем соответствующий список
+
+      if (!list || !list.classList.contains('services-card-list')) return;
+
+      // Если список уже активен, убираем класс
+      if (list.classList.contains('services-card-list-active')) {
+          list.classList.remove('services-card-list-active');
+      } else {
+          // Убираем класс у всех списков
+          document.querySelectorAll('.services-card-list').forEach(l => l.classList.remove('services-card-list-active'));
+          // Добавляем класс только соответствующему списку
+          list.classList.add('services-card-list-active');
+          button.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+  });
+});
+
